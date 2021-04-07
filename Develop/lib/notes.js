649b69f4a758f8f7df8ab19c4ad2bb729a31c4cc
Shawn Littrel <shawnlittrel@protonmail.com>
2021-04-07 14:50:34 -0700

    html routes working

---
+++ Develop/lib/notes.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+//What functionality do we need to have
+//Existing notes (db.json) populate on left hand container
+
+//When I enter a new note title and text, Save icon appears at the top of the page
+
+//When I click save icon, the note is saved and it populates in the left-hand container
+
+//When I click write icon, I am presented with emply fields to enter new note title and text
