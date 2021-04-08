18c13bd5b0de1861ed8ea88fada161dd7b703e17
Shawn Littrel <shawnlittrel@protonmail.com>
2021-04-07 18:43:58 -0700

    working on delete

--- Develop/lib/notes.js
+++ Develop/lib/notes.js
@@ -1,17 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-//What functionality do we need to have
-//Existing notes (db.json) populate on left hand container
-function displayNotes(){};
-
-
-//When I enter a new note title and text, Save icon appears at the top of the page
-     //This is included with the starter code
 
 //When I click save icon, the note is saved and it populates in the left-hand container
 function saveNotes(body, db){
      const noteBody = body;
+
      db.push(noteBody);
      fs.writeFileSync(
           path.join(__dirname, '../db/db.json'),
@@ -29,10 +23,21 @@ function validateNote(note) {
      }
      return true;
 };
-//When I click write icon, I am presented with emply fields to enter new note title and text
-function promptNewNote(){};
+
+//find object based on ID for deletion
+function findById(id, notesArray) {
+     const result = notesArray.filter(notes => notes.id === id)[0];
+     return result;
+}
+
+function deleteNote(id) {
+     let deletedNote = notesArray.findIndex(id)
+     console.log(deletedNote);
+}
 
 module.exports = {
                     saveNotes,
-                    validateNote
+                    validateNote,
+                    findById,
+                    deleteNote
                     }
--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -1,9 +1,8 @@
 const { notes } = require('../../db/db.json');
-
-const { saveNotes, validateNote } = require('../../lib/notes.js');
-
+const { saveNotes, validateNote, findById, deleteNote } = require('../../lib/notes.js');
 const router = require('express').Router();
-
+const { default: ShortUniqueId } = require('short-unique-id');
+const idNumber = new ShortUniqueId();
 router.get('/notes', (req, res) => {
      let results = notes;
 
@@ -11,8 +10,9 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-     req.body.id = notes.length.toString();
-
+     //generate unique ID for each entry and append to object
+     req.body.id = idNumber();
+     //only accept if all fields are populated
      if(!validateNote(req.body)) {
           res.status(400).send("Each note needs a title and text.");
      } else {
@@ -20,5 +20,19 @@ router.post('/notes', (req, res) => {
           res.json(note);
      }
 });
+
+// router.delete('/notes/:id', (req, res) => {
+//      const result = findById(req.params.id, notes).then
+     
+
+//      //on click, find id of clicked note
+//      //find index of element with matching id
+//      //delete index from array
+//      if (result) {
+//           res.json(result);
+//      } else {
+//           res.send(404);
+//      }
+// });
 
 module.exports = router;
